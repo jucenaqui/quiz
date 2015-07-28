fa7837e92772ac80100c2762e8d9ab5a0989f547
jucenaqui <juliocesarn2010@hotmail.com>
2015-07-28 15:37:44 -0500

    Agregado datos Autor desde rama Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,18 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* get /author*/
+
+router.get('/author', function(req,res){
+	res.render('author');
+});
+
 /* get /quizes/question*/
 router.get('/quizes/question', quizController.question);
 /* get /quizes/answer*/
 router.get('/quizes/answer', quizController.answer);
+
+
 
 
 module.exports = router;
